13f9c423cbfb5d128b1cd94c1aed07e484eef0b8
Abel Moremi <abelmoremiz@gmail.com>
2022-04-16 19:28:46 +0200

    create: basiccolor component

---
+++ src/components/BasicColor.vue
@@ -0,0 +1,18 @@
+<!-- add a full sceen color background -->
+<template>
+<div class='basic'>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- add background colour -->
+<style scoped>
+.basic {
+    background-color: #2c3e50;
+    padding: 100vh;
+}
+</style>
