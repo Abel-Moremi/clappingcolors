d8135dba0cf74f3119e7fcf689b6d475c80cbf2f
Abel Moremi <abelmoremiz@gmail.com>
2022-04-18 09:58:40 +0200

    add: uniqcolor package and random color swicth in the background

--- src/components/BasicColor.vue
+++ src/components/BasicColor.vue
@@ -1,18 +1,34 @@
 <!-- add a full sceen color background -->
 <template>
-<div class='basic'>
+<div class='basic' :style="{backgroundColor: color}">
 </div>
 </template>
 
 <script>
+import uniqolor from 'uniqolor';
+
 export default {
+    name: 'BasicColor',
+    data() {
+        return {
+            color: '#000000'
+        }
+    },
+    mounted() {
+        this.color = String(uniqolor.random().color);
+        console.log(this.color)
+    },
+    methods: {
+        changeColor() {
+            this.color = String(uniqolor.random().color);
+        }
+    }
 }
 </script>
 
 <!-- add background colour -->
 <style scoped>
 .basic {
-    background-color: #2c3e50;
     height: 100vh;
 }
 </style>
